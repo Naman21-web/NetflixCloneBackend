7bf2f3ad642a09274282eac2dd267f4dccde53c0
Naman21-web <82868154+Naman21-web@users.noreply.github.com>
2023-03-30 18:45:42 +0530

    m

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const userRoutes = require("./routes/UserRoutes");
+const userRoutes = require("./routes/userRoutes.js");
 const mongoose = require("mongoose");
 
 //use express
